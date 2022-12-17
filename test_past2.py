a6f60a958736942bcf1507c66033b402fa64d120
zhoujy1321 <78155196+zhoujy1321@users.noreply.github.com>
2022-12-17 21:06:56 +0800

    Delete test_past2.py

--- test_past2.py
+++
@@ -1,8 +0,0 @@
-import cmath
-a = float (input('input the value of a:'))
-b = float (input('input the value of b:'))
-c = float (input('input the value of c:'))
-delta = b**2 - 4*a*c
-res1 = (-b + cmath.sqrt(delta))/(2*a)
-res2 = (-b - cmath.sqrt(delta))/(2*a)
-print('the results are:{0} and {1}'.format(res1,res2))
