72e9a54780785e3da18e75e62d3d59ac365cf420
zhoujy1321 <78155196+zhoujy1321@users.noreply.github.com>
2022-12-18 19:51:11 +0800

    Add files via upload

---
+++ test_past7.py
@@ -0,0 +1,5 @@
+x = input('input the value of x:')
+y = input('input the value of y:')
+x,y = y,x
+print('the value of x after exchaging:{}'.format(x))
+print('the value of y after exchaging:{}'.format(y))
---
+++ test_past8.py
@@ -0,0 +1,7 @@
+year = int(input('input a year:'))
+if (year % 400) == 0:
+    print('the year is!')
+elif (year % 4) == 0:
+    print('the year is!')
+else:
+    print('the year isn\'t!')
